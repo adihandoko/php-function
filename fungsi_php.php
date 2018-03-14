758f6a0f6ff5d6786bb50b32dad60901fd7213da
adihandoko <adihandoko2@gmail.com>
2018-03-14 19:03:52 +0700

    fungsi rp

--- fungsi_php.php
+++ fungsi_php.php
@@ -1,4 +1,13 @@
 <?php
+function rupiah($angka){
+	
+	$hasil_rupiah = "Rp " . number_format($angka,2,',','.');
+	return $hasil_rupiah;
+ 
+}
+ 
+echo rupiah(1000000);
+
 $guid=make_GUID();
 echo $guid;
 
